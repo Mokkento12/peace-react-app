423564f92698d5eccec4f4637268d8410f1e1578
roman alekseev <roma.alekseev.90@bk.ru>
2024-11-08 18:45:50 +0300

    Добавлен компонент Promo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Hero from "./components/Hero/Hero";
+import Promo from "./components/Promo/Promo";
 
 function App() {
   return (
     <div className="App">
       <Hero />
+      <Promo />
     </div>
   );
 }
---
+++ src/components/Promo/Promo.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./Promo.css";
+
+const Promo = () => {
+  return (
+    <div class="promo">
+      <div class="promo-container">
+        <h1 class="promo-title">Find Your Best Psychic Advisor</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Promo;

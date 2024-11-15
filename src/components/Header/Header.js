5ec60c000c4328181a4475b72a5fce0420900bfb
roman alekseev <roma.alekseev.90@bk.ru>
2024-11-15 21:20:43 +0300

    Удалены классы активности: Убран код, который подсвечивал активный пункт меню. Логотип ведет на главную: Логотип теперь кликабелен и перенаправляет на главную страницу при нажатии. Нажатие на пункт меню вызывает переход: Каждый пункт меню теперь корректно вызывает функцию handleItemClick, которая использует navigate для перехода по нужному пути. Теперь при клике на пункты меню или на логотип будет происходить переход, и активный класс не будет изменяться.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,8 @@
 import React from "react";
-import { Link, useLocation, useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "./Header.css";
 
 const Header = () => {
-  const location = useLocation();
   const navigate = useNavigate();
 
   const handleItemClick = (path) => {
@@ -12,42 +11,36 @@ const Header = () => {
 
   return (
     <header className="header">
-      <a href="#home" className="logo">
+      <a href="#home" className="logo" onClick={() => handleItemClick("/")}>
         <img src="img/logo.png" alt="Logo" className="logo-img" />
       </a>
       <nav className="nav-menu">
         <ul className="header-menu">
-          <li
-            className={`header-list-item ${
-              location.pathname === "/" ? "active" : ""
-            }`}
-            onClick={() => handleItemClick("/")}
-          >
-            <Link to="/">Home</Link>
+          <li className="header-list-item">
+            <Link to="/" onClick={() => handleItemClick("/")}>
+              Home
+            </Link>
           </li>
-          <li
-            className={`header-list-item ${
-              location.pathname === "/client-registration" ? "active" : ""
-            }`}
-            onClick={() => handleItemClick("/client-registration")}
-          >
-            <Link to="/client-registration">Client Registration</Link>
+          <li className="header-list-item">
+            <Link
+              to="/client-registration"
+              onClick={() => handleItemClick("/client-registration")}
+            >
+              Client Registration
+            </Link>
           </li>
-          <li
-            className={`header-list-item ${
-              location.hash === "#advisors" ? "active" : ""
-            }`}
-            onClick={() => handleItemClick("#advisors")}
-          >
-            <a href="#advisors">Advisors</a>
+          <li className="header-list-item">
+            <a href="#advisors" onClick={() => handleItemClick("#advisors")}>
+              Advisors
+            </a>
           </li>
-          <li
-            className={`header-list-item ${
-              location.hash === "#register-advisor" ? "active" : ""
-            }`}
-            onClick={() => handleItemClick("#register-advisor")}
-          >
-            <a href="#register-advisor">Register as an Advisor</a>
+          <li className="header-list-item">
+            <a
+              href="#register-advisor"
+              onClick={() => handleItemClick("#register-advisor")}
+            >
+              Register as an Advisor
+            </a>
           </li>
         </ul>
       </nav>
